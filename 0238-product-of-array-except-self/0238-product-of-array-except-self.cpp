0469d6c40e72917c1ee99edbd3e7db669bfcce2b
Sahil Chhillar <12212237@nitkkr.ac.in>
2025-09-13 17:59:43 +0530

    Time: 6 ms (7.07%), Space: 43.7 MB (5.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        // int pos = 0;
-        // int size = nums.size();
-        // vector<int> result(size);
-        // for(int i = 0; i< size; i++ ){
-        //     int product = 1;
-        //     for(int j = 0;j<size;j++){
-        //         if(j != pos){
-        //             product *= nums[j];
-        //         }
-        //     }
-        //     result[pos++] = product;
-        // }
-        // return result;
         int n = nums.size();
-        vector<int> output(n);
-        output[0] = 1;
-        for(int i=1; i<n; i++){
-            output[i] = output[i-1] * nums[i-1];
+        vector<int> prefixPro(n,1);
+        for(int i = 1 ; i< n;i++){
+            prefixPro[i] = prefixPro[i-1] * nums[i-1];
         }
-        int right = 1;
-        for(int i=n-1; i>=0; i--){
-            output[i] *= right;
-            right *= nums[i];
+
+        vector<int> suffixPro(n,1);
+        for(int i = n-2;i >= 0;i--){
+            suffixPro[i] = suffixPro[i+1] * nums[i+1];
         }
-        return output;
+
+        vector<int> result;
+        for(int i = 0; i < n ; i++){
+            result.push_back(prefixPro[i] * suffixPro[i]);
+        }
+
+        return result;
     }
 };
