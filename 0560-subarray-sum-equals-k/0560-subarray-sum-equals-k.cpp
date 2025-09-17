c7be3745f2bc5f7f33bea610d2e50f1d6fa643e0
Sahil Chhillar <12212237@nitkkr.ac.in>
2025-09-17 17:40:34 +0530

    Time: 36 ms (85.05%), Space: 45.5 MB (55.3%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,12 +3,23 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
         int cnt = 0;
-        for(int i = 0 ; i< n; i++){
-            int currSum = 0;
-            for(int j = i ; j < n ; j++){
-                currSum += nums[j];
-                if(currSum == k) cnt++;
-            }
+        // for(int i = 0 ; i< n; i++){
+        //     int currSum = 0;
+        //     for(int j = i ; j < n ; j++){
+        //         currSum += nums[j];
+        //         if(currSum == k) cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        unordered_map<int,int> mp; // sum -> freq;
+        mp[0] = 1;
+        int currSum = 0;
+        for(auto& num : nums){
+            currSum += num;
+            int toFind = currSum - k;
+            if(mp.find(toFind) != mp.end()) cnt+= mp[toFind];
+            mp[currSum]++;
         }
         return cnt;
     }
