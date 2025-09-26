e0006778051e5fea9e977228b911c0e0b2b0d534
Sahil Chhillar <12212237@nitkkr.ac.in>
2025-09-26 23:10:23 +0530

    Time: 0 ms (100%), Space: 18.7 MB (33.95%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,47 +8,75 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* mergeTwo(ListNode* l1, ListNode* l2) {
+//         if (!l1) return l2;
+//         if (!l2) return l1;
+
+//         ListNode* head = nullptr;
+
+//         if (l1->val < l2->val) {
+//             head = l1;
+//             l1 = l1->next;
+//         }
+//         else{
+//             head = l2;
+//             l2 = l2->next;
+//         }
+
+//         ListNode* curr = head;
+//         while (l1 && l2) {
+//             if (l1->val < l2->val) {
+//                curr->next = l1;
+//                 l1 = l1->next;
+//             }
+//             else {
+//                 curr->next = l2;
+//                 l2 = l2->next;
+//             }
+//             curr = curr->next;
+//         }
+
+//         curr->next = (l1 ? l1 : l2);
+//         return head;
+//     }
+//     ListNode* mergeKLists(vector<ListNode*>& l) {
+//         if(l.empty()) return nullptr;
+
+//         ListNode* merged = l[0];
+//         for(int i = 1; i < l.size();i++){
+//             merged = mergeTwo(merged,l[i]);
+//         }
+
+//         return merged;
+//     }
+// };
 class Solution {
 public:
-    ListNode* mergeTwo(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
-        if (!l2) return l1;
+    typedef pair<int, ListNode*> p;
 
-        ListNode* head = nullptr;
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<p, vector<p>, greater<p>> pq;
+        ListNode* newHead = new ListNode(-1);
+        ListNode* temp = newHead;
 
-        if (l1->val < l2->val) {
-            head = l1;
-            l1 = l1->next;
-        } 
-        else{
-            head = l2;  
-            l2 = l2->next;
-        }
-
-        ListNode* curr = head;
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-               curr->next = l1;
-                l1 = l1->next;
-            } 
-            else {
-                curr->next = l2;
-                l2 = l2->next;
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists[i]) {
+                pq.push({lists[i]->val, lists[i]});
             }
-            curr = curr->next;
         }
 
-        curr->next = (l1 ? l1 : l2);
-        return head;
-    }
-    ListNode* mergeKLists(vector<ListNode*>& l) {
-        if(l.empty()) return nullptr;
+        while (!pq.empty()) {
+            auto p = pq.top();
+            pq.pop();
 
-        ListNode* merged = l[0];
-        for(int i = 1; i < l.size();i++){
-            merged = mergeTwo(merged,l[i]);
+            temp->next = p.second;
+            temp = temp->next;
+
+            if (p.second->next) pq.push({p.second->next->val, p.second->next});
         }
-        
-        return merged;
+
+        return newHead->next;
     }
 };
