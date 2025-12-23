a1875a993cb1fe6becbc85505a9a9b4a2edf8515
Sahil Chhillar <12212237@nitkkr.ac.in>
2025-12-23 17:41:00 +0530

    Time: 0 ms (100%), Space: 18 MB (61.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,16 +1,38 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int,int>freq;
+//         for(auto& num : nums) freq[num]++;
+
+//         vector<pair<int,int>> mostK; // {freq,num}
+//         for(auto& f : freq) mostK.push_back({f.second,f.first});
+//         sort(mostK.begin(),mostK.end());
+
+//         vector<int> ans;
+//         for(auto& p : mostK) ans.push_back(p.second);
+        
+//         return vector<int>(ans.end()-k,ans.end());
+//     }
+// };
+
+typedef pair<int,int> pii;
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>freq;
         for(auto& num : nums) freq[num]++;
 
-        vector<pair<int,int>> mostK; // {freq,num}
-        for(auto& f : freq) mostK.push_back({f.second,f.first});
-        sort(mostK.begin(),mostK.end());
+        priority_queue<pii,vector<pii>,greater<pii>> pq;
+        for(auto& f : freq){
+            pq.push({f.second,f.first});
+            if(pq.size()> k) pq.pop();
+        }
 
         vector<int> ans;
-        for(auto& p : mostK) ans.push_back(p.second);
-        
-        return vector<int>(ans.end()-k,ans.end());
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
     }
 };
