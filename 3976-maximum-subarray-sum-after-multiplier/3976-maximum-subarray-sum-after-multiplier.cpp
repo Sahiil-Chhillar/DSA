867b3cef3d6286c2069575b614448df23532c8ef
Sahil Chhillar <12212237@nitkkr.ac.in>
2026-06-28 12:07:32 +0530

    Time: 40 ms (67.04%), Space: 146.8 MB (69.13%) - LeetHub

---
+++ 3976-maximum-subarray-sum-after-multiplier/3976-maximum-subarray-sum-after-multiplier.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        long long res = -1e9, dp0 = 0, dpb = 0, dpc = 0, dp1 = 0;
+        for (long long a: nums) {
+            long long b = a * k, c = a / k;
+            dp1 = max({dp1 + a, dpb + a, dpc + a});
+            dpb = max({dp0 + b, dpb + b, b});
+            dpc = max({dp0 + c, dpc + c, c});
+            dp0 = max(dp0 + a, a);
+            res = max({res, dp0, dpb, dpc, dp1});
+        }
+        return res;
+    }
+};
