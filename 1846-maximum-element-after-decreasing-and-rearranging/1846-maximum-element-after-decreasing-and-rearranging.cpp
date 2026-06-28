33c9cb2c891d2819631577d3cc262e9cccbe97f0
Sahil Chhillar <12212237@nitkkr.ac.in>
2026-06-28 18:49:44 +0530

    Time: 3 ms (94.63%), Space: 55.3 MB (36.72%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -14,9 +14,7 @@ public:
         int n = A.size();
 
         A.front() = 1;
-        for (int i = 1; i < n; i++)
-            A[i] = min(A[i], A[i - 1] + 1);
-        
+        for (int i = 1; i < n; i++) A[i] = min(A[i], A[i - 1] + 1);
         return A.back();
     }
 };
