3d85d1c73dbca635446db7392458220e0d9dcff0
Sahil Chhillar <12212237@nitkkr.ac.in>
2026-07-17 23:00:43 +0530

    Time: 0 ms (100%), Space: 55 MB (86.11%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,14 +1,36 @@
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+
+//         int missingNum = 1;
+//         for(auto& num : nums) {
+//             if(num == missingNum) missingNum++;
+//             else if (num > missingNum) return missingNum;
+//         }
+
+//         return missingNum;
+//     }
+// };
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n = nums.size();
 
-        int missingNum = 1;
-        for(auto& num : nums) {
-            if(num == missingNum) missingNum++;
-            else if (num > missingNum) return missingNum;
+        // Put every number in its correct position i.e at index 0 -> 1 , 1 -> 2 and similarly so on 
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
 
-        return missingNum;
+        // Find the first incorrectly placed number
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+
+        return n + 1;
     }
 };
