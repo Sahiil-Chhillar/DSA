e4a01ad49d136f657639ef8f31f3a9758a37fb04
Sahil Chhillar <12212237@nitkkr.ac.in>
2026-07-17 22:55:54 +0530

    Time: 0 ms (100%), Space: 21.7 MB (71.02%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int actualSum = n*(n+1)/2;
+
+        int presentSum = 0;
+        for(auto& num : nums) presentSum += num;
+
+        return actualSum - presentSum;
+    }
+};
